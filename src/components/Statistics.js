fcfad6721d04324f6679521aa8ed8bc9ec8fcba7
mrc4619 <mrchayon5525@gmail.com>
2022-10-10 23:21:38 +0600

    statistics added

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { CartesianGrid, BarChart, Legend, Bar, Tooltip, XAxis, YAxis, } from 'recharts';
 
 const Statistics = () => {
+    const data = useLoaderData().data
     return (
         <div>
-            <h1>Statistics</h1>
-        </div>
+            <h1 className='text-center font-bold text-3xl text-gray-700 py-8'>The Quiz Statistics</h1>
+            <BarChart width={730} height={500} data={data}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="total" fill="#8884d8" />
+            </BarChart>
+        </div >
     );
 };
 
